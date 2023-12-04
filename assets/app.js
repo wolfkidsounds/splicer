0eed033984405dc22af924413f10ed4bd8a89fbf
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-04 05:03:58 +0100

    Bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,8 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
+// Node Modules
+import 'bootstrap';
 
-// any CSS you import will output into a single css file (app.css in this case)
+// SASS
+import './styles/app.scss';
+
+// CSS
 import './styles/app.css';
