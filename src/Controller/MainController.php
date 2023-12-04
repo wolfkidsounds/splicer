5b446182183680c624f1305df91b34a0790d9b98
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-04 04:54:34 +0100

    Update MainController.php

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class IndexController extends AbstractController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'app_index')]
     public function index(): Response
     {
         return $this->render('index.html.twig', [
-            'controller_name' => 'IndexController',
+            'controller_name' => 'MainController',
         ]);
     }
 }
