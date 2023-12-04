8fcf4fa021d2ab434cb2f656d9cd3f47342bdf3e
wolfkidsounds <32850639+wolfkidsounds@users.noreply.github.com>
2023-12-04 05:12:48 +0100

    Update Synth.php

--- src/Entity/Synth.php
+++ src/Entity/Synth.php
@@ -2,10 +2,12 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\SynthRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: SynthRepository::class)]
+#[ApiResource]
 class Synth
 {
     #[ORM\Id]
